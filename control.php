90e985685d6f29398b488e9dca58b2e95725aedd
Prabhas39971 <dakanaboinabrahmesh@gmail.com>
2024-08-16 15:50:06 +0530

    first git files

---
+++ control.php
@@ -0,0 +1,9 @@
+ <?php
+$age=18;
+if ($age < 20) {
+     echo "In this age your are the king in school.\n";
+     } elseif ($age >= 18 && $age < 21) {
+         echo "you are a major\n";
+     } else {
+         echo "You are an adult and  you can legally drink in the india.\n";
+     }
---
+++ operators.php
@@ -0,0 +1,7 @@
+<?php
+$num1=10;
+$num2=20
+$result = ($num1 == $num2); 
+var_dump("$result");
+echo "<br>";
+?>
